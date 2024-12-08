7620a3f6e8f5d6aaea9839c9ce459d3c674323fd
asujeet02 <asujeet02@gmail.com>
2024-12-08 15:18:09 +0530

    SQL configuration successfully established in Personal

--- src/main/java/com/cg/springsecurity/zero_to_master/config/ProjectSecurityConfig.java
+++ src/main/java/com/cg/springsecurity/zero_to_master/config/ProjectSecurityConfig.java
@@ -4,15 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,11 +31,9 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService()
+    public UserDetailsService userDetailsService(DataSource dataSource)
     {
-       UserDetails user= User.withUsername("SujeetK").password("{noop}zeroToone@12345").authorities("read").build();
-       UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$zluYhpmOvA.cJyKXCbm/s.NoL6cSAgmstT44nE4jt7YH3pDBbvVZy").authorities("admin").build();
-       return new InMemoryUserDetailsManager(user,admin);
+       return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,24 @@
+create table users(username varchar(50) not null primary key,password varchar(500) not null,enabled bit not null);
+create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on authorities (username,authority);
+
+IF NOT EXISTS (SELECT 1 FROM users WHERE username = 'SujeetK')
+BEGIN
+INSERT INTO users (username, password, enabled)
+VALUES ('SujeetK', '{noop}zeroToone@12345', '1');
+END
+
+IF NOT EXISTS (SELECT 1 FROM authorities WHERE username = 'SujeetK')
+BEGIN
+INSERT INTO authorities VALUES ('SujeetK','read');
+END
+
+IF NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin')
+BEGIN
+INSERT INTO users VALUES ('admin','{bcrypt}$2a$12$zluYhpmOvA.cJyKXCbm/s.NoL6cSAgmstT44nE4jt7YH3pDBbvVZy','1');
+END
+
+IF NOT EXISTS (SELECT 1 FROM authorities WHERE username = 'admin')
+BEGIN
+INSERT INTO authorities VALUES ('admin','admin');
+END
