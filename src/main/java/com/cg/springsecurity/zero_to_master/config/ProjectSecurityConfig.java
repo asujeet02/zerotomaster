456859e6b8c9d0b45bcb8368b8185e54de56d3cf
asujeet02 <asujeet02@gmail.com>
2024-12-10 21:19:30 +0530

    SQL configuration issue resolved perfectly and rest api created to add new customers in Database.

--- src/main/java/com/cg/springsecurity/zero_to_master/config/ProjectSecurityConfig.java
+++ src/main/java/com/cg/springsecurity/zero_to_master/config/ProjectSecurityConfig.java
@@ -4,37 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
-
+    
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
     {
         //http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/account","/balance","/cards","/loans").authenticated()
-                .requestMatchers("/notices","/contact","error").permitAll());
+        http.csrf(crsfConfig-> crsfConfig.disable()).authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/account","/balance","/cards","/loans").authenticated()
+                        .requestMatchers("/notices","/contact","error","/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(DataSource dataSource)
     {
        return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder()
