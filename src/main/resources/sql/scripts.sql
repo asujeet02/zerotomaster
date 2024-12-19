60e4e0a5257ff26c817e064a07b6f7dac5fbeded
asujeet02 <asujeet02@gmail.com>
2024-12-19 19:55:29 +0530

    SQL script modified and new tables added into database with new records

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -24,11 +24,120 @@ INSERT INTO authorities VALUES ('admin','admin');
 END
 
 CREATE TABLE customer (
-  id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  email varchar(45)  NOT NULL,
-  pwd varchar(200)  NOT NULL,
-  role varchar(45)  NOT NULL
+                          customer_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                          name varchar(100) NOT NULL,
+                          email varchar(100)  NOT NULL,
+                          mobile_number varchar(20) NOT NULL,
+                          pwd varchar(500)  NOT NULL,
+                          role varchar(100)  NOT NULL,
+                          create_dt date default null
 );
 
-insert into customer (email,pwd,role) values('happy@example.com','{noop}zeroToone@12345','read')
-insert into customer (email,pwd,role) values('admin@example.com','{bcrypt}$2a$12$zluYhpmOvA.cJyKXCbm/s.NoL6cSAgmstT44nE4jt7YH3pDBbvVZy','admin')
+insert into customer (name,email,mobile_number,pwd,role,create_dt) VALUES ('Happy','happy@example.com','5334122365', '{bcrypt}$2a$12$CIAvuhpGkJnS3BaiQ7Lomu9snma9xE8E4i2dTlsP1bIYbOmQwVn2W', 'admin',GETDATE());
+
+drop  table if exists accounts
+CREATE TABLE accounts (
+                          account_number int PRIMARY KEY NOT NULL,
+                          account_type varchar(100) NOT NULL,
+                          branch_address varchar(200) NOT NULL,
+                          create_dt date DEFAULT NULL,
+                          customer_id int not null,
+                          FOREIGN KEY(customer_id) REFERENCES customer (customer_id) ON DELETE CASCADE
+);
+
+INSERT INTO accounts (customer_id, account_number, account_type, branch_address, create_dt)
+VALUES (1, 1865764534, 'Savings', '123 Main Street, New York', GETDATE());
+
+CREATE TABLE account_transactions (
+                                      transaction_id varchar(200) PRIMARY KEY NOT NULL,
+                                      transaction_dt date NOT NULL,
+                                      transaction_summary varchar(200) NOT NULL,
+                                      transaction_type varchar(100) NOT NULL,
+                                      transaction_amt int NOT NULL,
+                                      closing_balance int NOT NULL,
+                                      create_dt date DEFAULT NULL,
+                                      account_number int NOT NULL,
+                                      customer_id int NOT NULL,
+                                      FOREIGN KEY (account_number) REFERENCES accounts (account_number) ON DELETE CASCADE,
+                                      FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE NO ACTION
+);
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -7, GETDATE()), 'Coffee Shop', 'Withdrawal', 30, 34500, DATEADD(DAY, -7, GETDATE()));
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -6, GETDATE()), 'Uber', 'Withdrawal', 100, 34400, DATEADD(DAY, -6, GETDATE()));
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -5, GETDATE()), 'Self Deposit', 'Deposit', 500, 34900, DATEADD(DAY, -5, GETDATE()));
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -4, GETDATE()), 'Ebay', 'Withdrawal', 600, 34300, DATEADD(DAY, -4, GETDATE()));
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -2, GETDATE()), 'OnlineTransfer', 'Deposit', 700, 35000, DATEADD(DAY, -2, GETDATE()));
+
+INSERT INTO account_transactions (transaction_id, account_number, customer_id, transaction_dt, transaction_summary, transaction_type, transaction_amt,
+                                  closing_balance, create_dt)
+VALUES (NEWID(), 1865764534, 1, DATEADD(DAY, -1, GETDATE()), 'Amazon.com', 'Withdrawal', 100, 34900, DATEADD(DAY, -1, GETDATE()));
+
+
+CREATE TABLE loans (
+                       loan_number int IDENTITY(1,1) PRIMARY KEY NOT NULL ,
+                       customer_id int NOT NULL,
+                       start_dt date NOT NULL,
+                       loan_type varchar(100) NOT NULL,
+                       total_loan int NOT NULL,
+                       amount_paid int NOT NULL,
+                       outstanding_amount int NOT NULL,
+                       create_dt date DEFAULT NULL,
+                       CONSTRAINT loan_customer_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE CASCADE
+);
+
+INSERT INTO loans ( customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt)
+VALUES ( 1, '2020-10-13', 'Home', 200000, 50000, 150000, '2020-10-13');
+
+INSERT INTO loans ( customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt)
+VALUES ( 1, '2020-06-06', 'Vehicle', 40000, 10000, 30000, '2020-06-06');
+
+INSERT INTO loans ( customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt)
+VALUES ( 1, '2018-02-14', 'Home', 50000, 10000, 40000, '2018-02-14');
+
+INSERT INTO loans ( customer_id, start_dt, loan_type, total_loan, amount_paid, outstanding_amount, create_dt)
+VALUES ( 1, '2018-02-14', 'Personal', 10000, 3500, 6500, '2018-02-14');
+
+CREATE TABLE cards (
+                       card_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                       card_number varchar(100) NOT NULL,
+                       customer_id int NOT NULL,
+                       card_type varchar(100) NOT NULL,
+                       total_limit int NOT NULL,
+                       amount_used int NOT NULL,
+                       available_amount int NOT NULL,
+                       create_dt date DEFAULT NULL,
+                       CONSTRAINT card_customer_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE CASCADE
+);
+
+INSERT INTO cards (card_number, customer_id, card_type, total_limit, amount_used, available_amount, create_dt)
+VALUES ('4565XXXX4656', 1, 'Credit', 10000, 500, 9500, GETDATE());
+
+INSERT INTO cards (card_number, customer_id, card_type, total_limit, amount_used, available_amount, create_dt)
+VALUES ('3455XXXX8673', 1, 'Credit', 7500, 600, 6900, GETDATE());
+
+INSERT INTO cards (card_number, customer_id, card_type, total_limit, amount_used, available_amount, create_dt)
+VALUES ('2359XXXX9346', 1, 'Credit', 20000, 4000, 16000, GETDATE());
+
+CREATE TABLE notice_details (
+                                notice_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                                notice_summary varchar(200) NOT NULL,
+                                notice_details varchar(500) NOT NULL,
+                                notic_beg_dt date NOT NULL,
+                                notic_end_dt date DEFAULT NULL,
+                                create_dt date DEFAULT NULL,
+                                update_dt date DEFAULT NULL,
+);
